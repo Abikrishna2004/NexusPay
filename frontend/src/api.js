de8551621f5a40ed65331c02155533570c294543
Abikrishna2004 <abikrishna04edu@gmail.com>
2026-03-23 15:48:48 +0530

    final commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://nexuspay-tz1s.onrender.com/api',
+    baseURL: 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use((config) => {
