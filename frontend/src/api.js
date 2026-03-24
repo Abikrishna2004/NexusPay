31e79ba86ea45c3611ceec743864ec70a55e538d
Abikrishna2004 <abikrishna04edu@gmail.com>
2026-03-24 14:01:26 +0530

    currency change

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://nexus-pay-alpha.vercel.app/api',
+    baseURL: window.location.hostname === 'localhost' ? 'http://localhost:5000/api' : 'https://nexus-pay-alpha.vercel.app/api',
 });
 
 api.interceptors.request.use((config) => {
